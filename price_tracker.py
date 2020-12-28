89112f232c1acbc4e51e1097eab57e5bfacb1023
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2020-12-28 18:31:31 +0530

    Initial Commit

--- price_tracker.py
+++ price_tracker.py
@@ -1,23 +1,24 @@
+# import the required packages
 import requests
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 
-account_sid = "AC7e50d4f036713c2d80a7fc5837c03de2"
-auth_token = "04847618441ed3b66e70a91a0d04d7a7"
+# Your Twilio account Details
+account_sid = "Your Twilio account sid "
+auth_token = "Your Twilio auth token"
 client = Client(account_sid, auth_token)
 
-URL = "https://www.amazon.in/Apple-MacBook-Pro-8th-Generation-Intel-Core-i5/dp/B07SDPTPC6/"
+URL = "https://www.amazon.in/Apple-MacBook-Pro-8th-Generation-Intel-Core-i5/dp/B07SDPTPC6/" # Url of the website you want to do webscrapping
 
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66"
 }
 
-
-def send_message(title,price):
+def send_message(title,price): # this function sends text messages to respective phone number
     message = client.messages.create(
-        from_= '+16592013973',
-        body= "Price Drop Detected {} in{}".format(title, price),
-        to='+919987882211',
+        from_= '+16592013973', #Enter Your Twilio phone no
+        body= "Price Drop Detected {} in{}".format(title, price), # The message you want to send
+        to='+911234567890', #Enter the number you wan to send message to 
     )
     print(message.sid)
 
@@ -26,14 +27,14 @@ def price_tracker():
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    title = soup.find(id="productTitle").get_text().strip()
+    title = soup.find(id="productTitle").get_text().strip() # finds the element by its respective id 
 
-    price = soup.find(id="priceblock_ourprice").get_text().strip(",")[2:].split(".")[0]
+    price = soup.find(id="priceblock_ourprice").get_text().strip(",")[2:].split(".")[0] # finds the element by its respective id 
 
-    coverted_price = int(price.replace(",", "")) 
+    coverted_price = int(price.replace(",", "")) # replaces the "," or removes the commas
 
     if coverted_price <= 165000:
-        send_message(title, price)
+        send_message(title, price) # If fall in price detected the send_message function is called 
     
     else: 
         print("No Price Drop Detected")
